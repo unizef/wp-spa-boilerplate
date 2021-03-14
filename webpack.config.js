991fd88bb322de018b3305e99eab581893d5cf56
unizef <zefstribos@gmail.com>
2021-03-14 12:32:41 +0100

    enabled source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
-module.exports = {
-  mode: "development",
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
 
-  devtool: false,
+module.exports = {
+  mode: mode,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
   devServer: {
     contentBase: "./dist",
   },
